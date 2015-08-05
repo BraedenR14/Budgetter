2d42a46b82ba92ddbcd4a34e80146dc7942b52d7
braeden <Octopu@Peanuts-MacBook-Pro.local>
2015-08-05 15:33:33 -0600

    Initial Commit

---
+++ Budgetter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Budgetter
+//
+//  Created by braeden on 2015-08-05.
+//  Copyright (c) 2015 braeden. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Budgetter/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Budgetter
+//
+//  Created by braeden on 2015-08-05.
+//  Copyright (c) 2015 braeden. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
